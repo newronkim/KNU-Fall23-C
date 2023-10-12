dfd17a1d052f836712628b71f31e0c6e9539c2cf
newronkim <144310846+newronkim@users.noreply.github.com>
2023-10-12 16:19:39 +0900

    Update lecture3.cpp

--- KNU-C/20231012/lecture3.cpp
+++ KNU-C/20231012/lecture3.cpp
@@ -2,14 +2,23 @@
 int main() {
 	int score[5] = { 87, 92, 89, 98, 78 };
 
-	printf("score[5] = {87, 92, 89, 9, 78} 배열 요소 출력\n");
+	printf("score[5] = {87, 92, 89, 98, 78} 배열 요소 출력\n");
 
 	int* p_score = score;
 
 	for (int i = 0; i < 5; i++) {
-		printf("score[%d] = [%d]\n", i, *p_score);
+		printf("%d\n", i, *p_score);
 		p_score++;
 	}	
 
 	return 0;
 }
+
+/*  세 번 반복
+	int* p_score = score+1;
+	int* p_score = &score[1];
+
+	for (int i = 0; i < 3; i++) {
+		printf("%d\n", i, *p_score);
+		p_score++;
+}*/
